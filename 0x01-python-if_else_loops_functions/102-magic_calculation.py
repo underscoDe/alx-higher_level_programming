e7cd7434217f76bb879781cf007351f88fdf3bc2
underscoDe <developer.awounou@gmail.com>
2021-11-17 23:41:17 +0100

    ByteCode -> Python #2

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def magic_calculation(a, b, c):
-    """Match bytecode provided by Holberton School."""
     if a < b:
         return (c)
     if c > b:
