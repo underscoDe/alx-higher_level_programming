10bc4d8cfa84c967bbdf4b128bd0e6b7890a195c
underscoDe <developer.awounou@gmail.com>
2021-11-18 07:39:15 +0100

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
