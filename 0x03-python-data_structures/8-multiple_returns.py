6b205ff4ed79bd60da13a43267bb8684d6f3131d
underscoDe <developer.awounou@gmail.com>
2021-11-21 15:13:16 +0100

    multiple returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -13,4 +13,7 @@ def multiple_returns(sentence):
         A tuple
     """
 
-    return (0, None) if len(sentence) == 0 else len(sentence), sentence[0]
+    if len(sentence) == 0:
+        return (0, None)
+    else:
+        return len(sentence), sentence[0]
