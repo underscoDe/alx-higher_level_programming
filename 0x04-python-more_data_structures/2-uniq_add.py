f7fa239c9e2d50f3d46fbcc5f5d8583e168c85af
underscoDe <developer.awounou@gmail.com>
2021-11-25 00:35:36 +0100

    Unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,18 @@
+def uniq_add(my_list=[]):
+    """
+    Adds all unique integers in a list (only once for each integer)
+    ...
+
+    Parameters
+    ----------
+    my_list : list
+        List of elements
+
+    Return:
+        the result of the addition
+    """
+
+    result = 0
+    for x in set(my_list):
+        result += x
+    return (result)
