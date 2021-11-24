0c1c2e6db7c204c860030e7f3a5dc1f8b82fb5d3
underscoDe <developer.awounou@gmail.com>
2021-11-25 00:45:49 +0100

    Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,15 @@
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary
+    ...
+
+    Parameters
+    ----------
+    a_dictionary : dictionary
+        the given dictionary
+
+    Return:
+        the number of keys
+    """
+
+    return (len(a_dictionary))
