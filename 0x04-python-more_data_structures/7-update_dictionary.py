c6e8156bea140273a2df057c92776674196460d6
underscoDe <developer.awounou@gmail.com>
2021-11-25 00:53:12 +0100

    Update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,20 @@
+def update_dictionary(a_dictionary, key, value):
+    """
+    Replaces or adds key/value in a dictionary
+    ...
+
+    Parameters
+    ----------
+    a_dictionary : dictionary
+        the given dictionary
+    key : string
+        argument will be always a string
+    value : any type
+        argument will be any type
+
+    Return:
+        the dictionary with the new inserted value
+    """
+
+    a_dictionary[key] = value
+    return (a_dictionary)
