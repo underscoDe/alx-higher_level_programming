adc6fdc235757ce17e5903166e7f8106d0cf1dc9
underscoDe <developer.awounou@gmail.com>
2021-11-25 00:24:21 +0100

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,23 @@
+def search_replace(my_list, search, replace):
+    """
+    Replaces all occurrences of an element by another in a new list
+    ...
+
+    Parameters
+    ----------
+    my_list : list
+        Initial list of elements
+    search : integer
+        The element to replace in the list
+    replace : integer
+        The new element
+
+    Return:
+        a new list with the new elements
+    """
+
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
