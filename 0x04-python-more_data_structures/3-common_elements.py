91883969b76ed8ded48a2cfb4eca2c0ffc87238d
underscoDe <developer.awounou@gmail.com>
2021-11-25 00:37:34 +0100

    Present in both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,17 @@
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements in two sets
+    ...
+
+    Parameters
+    ----------
+    set_1 : set
+        first set of elements
+    set_2 : set
+        second set of elements
+
+    Return:
+        the result of the operation (&)
+    """
+
+    return (set_1 & set_2)
