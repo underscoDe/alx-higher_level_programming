34fc41ae0d954d8b49830945c6d7d13e9bc3701b
underscoDe <developer.awounou@gmail.com>
2021-11-25 01:07:23 +0100

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    """
+    Returns a key with the biggest integer value
+    ...
+
+    Parameters
+    ----------
+    a_dictionary : dictionary
+        the given dictionary
+
+    Return:
+        If no score found, return None
+        Key with biggest integer value otherwise
+    """
+
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
