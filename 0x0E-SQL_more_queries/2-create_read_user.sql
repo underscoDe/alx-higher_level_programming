f1ef9506bc936e8cb35f9aa4fa7ac92da6122894
underscoDe <developer.awounou@gmail.com>
2022-02-05 12:31:02 +0100

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,9 @@
 -- Creates the database hbtn_0d_2 and the user user_0d_2
 -- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
+    ON hbtn_0d_2.*
+    TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
