503910905b30cd34cfa3d9ad7ee1abb8aed82f98
underscoDe <developer.awounou@gmail.com>
2022-03-02 02:13:32 +0100

    Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
+const newDict = {};
+for (const key in dict) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [key];
   } else {
