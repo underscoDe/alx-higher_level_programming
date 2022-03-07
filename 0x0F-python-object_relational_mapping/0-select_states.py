6ec1e1ff70f7a6f8db13cbdf53b128f2560b6601
underscoDe <developer.awounou@gmail.com>
2022-03-07 01:25:20 +0100

    imports order

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,11 @@
 # Usage: ./0-select_states.py <mysql username> \
 #                             <mysql password> \
 #                             <database name>
-import sys
-import MySQLdb
-
 if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+
     db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states` ORDER BY `states.id` ASC")
