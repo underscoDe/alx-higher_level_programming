1eaef1e36dc81b76909e6ecd85fe8267d103fcd1
underscoDe <developer.awounou@gmail.com>
2022-03-07 01:41:07 +0100

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
-# Usage: ./1-filter_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+Usage: ./1-filter_states.py <mysql username> \
+                            <mysql password> \
+                            <database name>
+"""
+
 import sys
 import MySQLdb
 
