6db666ddc99352713f33bcb890f2df9603f87db4
underscoDe <developer.awounou@gmail.com>
2022-04-14 00:44:05 +0100

    semistandard coding style

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
