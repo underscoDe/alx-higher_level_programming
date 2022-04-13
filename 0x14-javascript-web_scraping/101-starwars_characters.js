9879b5c8feae5ea802659af9fe5d690193b8206f
underscoDe <developer.awounou@gmail.com>
2022-04-14 00:41:41 +0100

    https://swapi-api.hbtn.io/api/films/

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (!error) {
     let characters = JSON.parse(body).characters;
